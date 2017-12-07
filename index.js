e966402cbd57484fc7549a96f57bd5ef071e4a44
dubclickdev <battim_3c@yahoo.com>
2017-12-07 19:53:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){
-  var newObject = Object.assign({}, object, {[key]: value})
-  return newObject
-}
-function updateObjectWithKeyAndValue(object, key, value){
   var newPair = {[key]: value}
   var newObject = Object.assign({}, object, newPair)
   return newObject
