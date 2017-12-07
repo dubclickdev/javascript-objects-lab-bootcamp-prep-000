959e9933e124087e8c7a5bdda1ccd1825f5ec693
dubclickdev <battim_3c@yahoo.com>
2017-12-07 19:50:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   var newObject = Object.assign({}, object, newPair)
   return newObject
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
